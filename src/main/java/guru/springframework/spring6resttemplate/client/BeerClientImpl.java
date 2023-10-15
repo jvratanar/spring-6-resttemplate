b123f1d20819552d6a630bf78e7071edd35563cd
Janez Franc Vratanar <jvratanar@gmail.com>
2023-10-15 12:19:25 +0200

    Section 19, Chapter 196

---
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -0,0 +1,28 @@
+package guru.springframework.spring6resttemplate.client;
+
+import guru.springframework.spring6resttemplate.model.BeerDTO;
+import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class BeerClientImpl implements BeerClient {
+    private final RestTemplateBuilder restTemplateBuilder;
+
+    private static final String BASE_URL = "http://localhost:8080";
+    private static final String GET_BEER_PATH = "/api/v1/beer";
+
+    @Override
+    public Page<BeerDTO> listBeers() {
+        RestTemplate restTemplate = this.restTemplateBuilder.build();
+        ResponseEntity<BeerDTOPageImpl> stringResponse =
+                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, BeerDTOPageImpl.class);
+
+        return null;
+    }
+}
