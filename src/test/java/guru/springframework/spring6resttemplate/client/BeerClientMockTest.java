e590adbff085a2f80b7f2eb00af890690e740924
Janez Franc Vratanar <jvratanar@gmail.com>
2023-10-18 19:27:42 +0200

    Section 20, Chapter 211.

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientMockTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientMockTest.java
@@ -19,16 +19,18 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.math.BigDecimal;
+import java.net.URI;
 import java.util.Arrays;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.*;
 
 @RestClientTest
 @Import(RestTemplateBuilderConfig.class)
@@ -49,12 +51,43 @@ public class BeerClientMockTest {
     @Mock
     RestTemplateBuilder mockRestTemplateBuilder = new RestTemplateBuilder(new MockServerRestTemplateCustomizer());
 
+    BeerDTO dto;
+
+    String dtoJson;
+
     @BeforeEach
-    void setUp() {
+    void setUp() throws JsonProcessingException {
         RestTemplate restTemplate = this.restTemplateBuilderConfigured.build();
         this.server = MockRestServiceServer.bindTo(restTemplate).build();
         when(mockRestTemplateBuilder.build()).thenReturn(restTemplate);
         this.beerClient = new BeerClientImpl(this.mockRestTemplateBuilder);
+        this.dto = this.getBeerDto();
+        this.dtoJson = objectMapper.writeValueAsString(this.dto);
+    }
+
+    @Test
+    void testUpdateBeer() {
+        this.server.expect(method(HttpMethod.PUT))
+                .andExpect(requestToUriTemplate(URL + BeerClientImpl.GET_BEER_BY_ID_PATH, dto.getId()))
+                .andRespond(withNoContent());
+        this.mockGetOperation();
+
+        BeerDTO responseDto = this.beerClient.updateBeer(this.dto);
+        assertThat(responseDto.getId()).isEqualTo(this.dto.getId());
+    }
+
+    @Test
+    void testCreateBeer() {
+        URI uri = UriComponentsBuilder.fromPath(BeerClientImpl.GET_BEER_BY_ID_PATH).build(dto.getId());
+
+        this.server.expect(method(HttpMethod.POST))
+                .andExpect(requestTo(URL + BeerClientImpl.GET_BEER_PATH))
+                .andRespond(withAccepted().location(uri));
+
+        this.mockGetOperation();
+
+        BeerDTO responseDto = this.beerClient.createBeer(this.dto);
+        assertThat(responseDto.getId()).isEqualTo(this.dto.getId());
     }
 
     @Test
@@ -67,6 +100,20 @@ public class BeerClientMockTest {
 
         Page<BeerDTO> dtos = this.beerClient.listBeers();
         assertThat(dtos.getContent().size()).isGreaterThan(0);
+    }
+
+    @Test
+    void testGetBeerById() {
+        this.mockGetOperation();
+
+        BeerDTO responseDto = this.beerClient.getBeerById(this.dto.getId());
+        assertThat(responseDto.getId()).isEqualTo(this.dto.getId());
+    }
+
+    private void mockGetOperation() {
+        this.server.expect(method(HttpMethod.GET))
+                .andExpect(requestToUriTemplate(URL + BeerClientImpl.GET_BEER_BY_ID_PATH, dto.getId()))
+                .andRespond(withSuccess(this.dtoJson, MediaType.APPLICATION_JSON));
     }
 
     BeerDTO getBeerDto(){
